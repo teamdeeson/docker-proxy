f66402d9cf0bccecf8bce17953ffb9d3cbfe730d
Adam Dowling <adamd@deeson.co.uk>
2018-06-01 01:47:28 +0100

    Tidy up to start.sh

--- start.sh
+++ start.sh
@@ -10,16 +10,4 @@ fi
 ./genlocalcrt.sh ./.certs
 
 docker network create proxy
-docker run -d \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v $PWD/traefik.toml:/traefik.toml \
-  -v $PWD/.certs:/certs \
-  -p 80:80 \
-  -p 443:443 \
-  -l traefik.frontend.rule=Host:monitor.localhost \
-  -l traefik.port=8080 \
-  --network proxy \
-  --name traefik \
-  traefik:1.3.6-alpine --docker
-
-#-v $PWD/acme.json:/acme.json \
+docker-compose up -d
